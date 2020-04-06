6a17c77a0a70fe440ae41d10fa55c34138ed5ba8
nelsonic <nelson+github@dwyl.io>
2020-04-06 21:11:06 +0100

    create test/app/verify_request_test.exs

---
+++ test/app/verify_request_test.exs
@@ -0,0 +1,9 @@
+defmodule App.VerifyRequestTest do
+  use ExUnit.Case
+  alias App.Plug.VerifyRequest
+
+  test "invoke init/1" do
+    assert VerifyRequest.init(%{hello: "world"}) == %{hello: "world"}
+  end
+
+end
